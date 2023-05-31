33034f4e451cd0a9c948154bcb66c55fbaa18b2f
Kevin <86487929+tar83@users.noreply.github.com>
2023-05-30 19:37:30 -0500

    sidebarColumns ya no es necesario

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -7,11 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
   completeSideBar: boolean;
-  sidebarColumns: number;
 
   constructor() {
     this.completeSideBar = true;
-    this.sidebarColumns = 5;
   }
 
   ngOnInit(): void {}
