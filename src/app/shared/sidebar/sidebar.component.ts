2885730e3bb39a7da2cd8338dc68aaa6b4359df1
Kevin <86487929+tar83@users.noreply.github.com>
2023-05-30 19:28:56 -0500

    Sidebar agregada

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  completeSideBar: boolean;
+  sidebarColumns: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.completeSideBar = true;
+    this.sidebarColumns = 5;
   }
 
+  ngOnInit(): void {}
+
+  modifySideBarView() {
+    this.completeSideBar = !this.completeSideBar;
+  }
 }
