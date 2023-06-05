cc5a6a71e6bf52327e92d56f18ae46d79e0598fe
Kevin <86487929+tar83@users.noreply.github.com>
2023-06-04 20:07:52 -0500

    funcionalidad basica para mostrar fichas medicas individuales

--- src/app/pages/fichas-medicas/fichas-medicas.module.ts
+++ src/app/pages/fichas-medicas/fichas-medicas.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 import { Route, RouterLinkWithHref, RouterModule } from '@angular/router';
 import { FichasMedicasHomeComponent } from './home/home.component';
 import { FichaMedicaComponent } from './ficha-medica/ficha-medica.component';
+import { DetallesComponent } from './detalles/detalles.component';
 
-const routes: Route[] = [{ path: '', component: FichasMedicasHomeComponent }];
+const routes: Route[] = [
+  { path: '', component: FichasMedicasHomeComponent, title: 'Fichas Médicas' },
+  { path: 'detalles/:id', component: DetallesComponent, title: 'Detalle' },
+];
 
 @NgModule({
-  declarations: [FichasMedicasHomeComponent, FichaMedicaComponent],
+  declarations: [
+    FichasMedicasHomeComponent,
+    FichaMedicaComponent,
+    DetallesComponent,
+  ],
   imports: [CommonModule, RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
